9299ca159a2a32c6b97d78d161b206e9317a34b1
alexanderguk <alexander-v-guk@yandex.ru>
2020-02-19 14:24:06 +0300

    done!

--- translater.js
+++ translater.js
@@ -8,28 +8,51 @@ http.createServer(async (request, response) => {
         body.push(chunk);
     }).on('end', async () => {
         body = Buffer.concat(body).toString();
-        const translateUrl = "https://api.cognitive.microsofttranslator.com/translate?api-version=3.0&to=ru";
-        let translatedArray = [];
-        const msTranslateBody = {
-            body: `[{'Text':'${body}'}]`,
-            headers: {
-                "Content-Type": "application/json; charset=UTF-8",
-                "Ocp-Apim-Subscription-Key": "7fda0040c26140768975d7ca6dcbbfcd"
-            },
-            method: "POST"
-        };
-        let translatedObject = await fetch(translateUrl, msTranslateBody).then(response => response.json());
-        if ( translatedObject.error ) {
-            response.statusCode = 400;
-            response.end(translatedObject.error.message);
+        let translatedArray = await getTranslateFromGoogle(body);
+        if ( translatedArray ) {
+            console.log("from google");
+            console.log(JSON.stringify(translatedArray));
+            response.end(JSON.stringify(translatedArray.join(" ")));
+            return;
         }
-        if ( Array.isArray(translatedObject) && translatedObject[0] && translatedObject[0].translations ) {
-            translatedArray = translatedObject[0].translations.map(translate => translate.text);
-            console.warn(translatedArray);
-            response.end(JSON.stringify(translatedArray));
+        translatedArray = await getTranslateFromBing(body);
+        if ( translatedArray ) {
+            console.log("from bing");
+            console.log(JSON.stringify(translatedArray));
+            response.end(JSON.stringify(translatedArray.join(" ")));
+            return;
         }
     });
-
-    //response.statusCode = 404;
-   // response.end("ERROR");
 }).listen(3000, "localhost", () => console.log("started server"));
+
+async function getTranslateFromGoogle(text) {
+    const encodedText = encodeURIComponent(text);
+    const reqUrl = `https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=ru&hl=ru&dt=t&dt=bd&dj=1&source=input&q=${encodedText}`;
+    const response = await fetch(reqUrl).then(resp => resp.ok ? resp.json() : null).catch(e => null);
+    if ( !response ) {
+        return null;
+    }
+    return response.sentences.map(sentence => sentence.trans);
+}
+
+async function getTranslateFromBing(text) {
+    const encodedText = encodeURIComponent(text);
+    const reqUrl = "https://api.cognitive.microsofttranslator.com/translate?api-version=3.0&to=ru";
+    let translatedArray = [];
+    const msTranslateBody = {
+        body: `[{'Text':'${encodedText}'}]`,
+        headers: {
+            "Content-Type": "application/json; charset=UTF-8",
+            "Ocp-Apim-Subscription-Key": "7fda0040c26140768975d7ca6dcbbfcd"
+        },
+        method: "POST"
+    };
+    let translatedObject = await fetch(reqUrl, msTranslateBody).then(response => response.ok ? response.json() : null).catch(e => null);
+    if ( translatedObject.error ) {
+        return null;
+    }
+    if ( Array.isArray(translatedObject) && translatedObject[0] && translatedObject[0].translations ) {
+        translatedArray = translatedObject[0].translations.map(translate => translate.text);
+        return translatedArray;
+    }
+}
